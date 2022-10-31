8094d7650a9c1d70adbe187581ae56dfe02796d4
avishay avraham <avishayavraham555@gmail.com>
2022-10-31 11:33:47 +0200

    sections done

--- src/Components/Contact/ContactComp.jsx
+++ src/Components/Contact/ContactComp.jsx
@@ -1,35 +1,75 @@
-import React from 'react'
-import "./contact.css"
-import {MdOutlineEmail} from "react-icons/md"
-import {BsWhatsapp} from "react-icons/bs"
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const ContactComp = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_hm6upvi",
+      "template_r6ui06j",
+      form.current,
+      "rr2P-GhFRglj6K8gv"
+    );
+
+    e.target.reset();
+  };
+
   return (
-    <section id='contact'>
+    <section id="contact">
       <h2>Contact Me</h2>
 
       <div className="container contact-container">
         <div className="contact-options">
           {/* email option */}
           <article className="contact-option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className="contact-option-icon" />
             <h4>Email</h4>
             <h5>avishay99a@gmail.com</h5>
-            <a href='mailto:avishay99a@gmail.com'>Send a message</a>
+            <a href="mailto:avishay99a@gmail.com" target="_blank">
+              Send a message
+            </a>
           </article>
           {/* whatsapp option */}
           <article className="contact-option">
-            <BsWhatsapp/>
+            <BsWhatsapp className="contact-option-icon" />
             <h4>Whatsapp</h4>
             <h5>+972***</h5>
-            <a href='http://api.whatsapp.com/send?phone+9720504335393'>Send a message</a>
+            <a
+              href="http://api.whatsapp.com/send?phone=+9720504335393"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </article>
         </div>
         {/* end of contact options */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactComp
+export default ContactComp;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./footer.css"
+import {BsLinkedin} from "react-icons/bs"
 
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <footer>
+      <a href='#' className='footer-logo'>ATLAS</a>
+
+      <ul className="permalinks">
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href='https://linkedin.com'><BsLinkedin/></a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>&copy; ATLAS Design. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
