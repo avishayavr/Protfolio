ec63634e5e8c2d3736d86197884bd357b9462cdd
avishay avraham <avishayavraham555@gmail.com>
2023-02-16 18:50:13 +0200

    project 1 ready

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./projects.css"
-import img1 from "../../assets/project1.png"
+import img1 from "../../assets/project2.png"
+const data = [{url:img1,title:"Ecommerce", gitHub:"https://github.com/avishayavr/protfolio", liveDemo:"https://anime-products.vercel.app/"}]
 
 
 const Projects = () => {
@@ -9,17 +10,22 @@ const Projects = () => {
       <h2>My Projects</h2>
 
       <div className="container projects-container">
-        <article className="project-item">
+        {data.map(project=>{
+        return(
+          <article className="project-item">
           <div className="project-item-image">
-            <img src={img1} alt="" />
+            <img src={project.url} alt="" />
           </div>
 
-          <h3>TV-Shows</h3>
+          <h3>{project.title}</h3>
           <div className="project-item-cta">
-          <a href='' className='btn' target="_blank">Github</a>
-          <a href='' className='btn btn-primary' target="_blank">Live Demo</a>
+          <a href={project.gitHub} className='btn' target="_blank">Github</a>
+          <a href={project.liveDemo} className='btn btn-primary' target="_blank">Live Demo</a>
           </div>
         </article>
+        )
+        })}
+        
       </div>
     </section>
   )
