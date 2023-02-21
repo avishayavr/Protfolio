718d230db6a72fc9e00a85f4ea8bb4311969f1fb
avishay avraham <avishayavraham555@gmail.com>
2023-02-21 15:05:01 +0200

    project 2 done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./projects.css"
 import img1 from "../../assets/project2.png"
-const data = [{url:img1,title:"Ecommerce", gitHub:"https://github.com/avishayavr/protfolio", liveDemo:"https://anime-products.vercel.app/"}]
+import img2 from "../../assets/project1.png"
+const data = [{url:img1,title:"Ecommerce", gitHub:"https://github.com/avishayavr/protfolio", liveDemo:"https://anime-products.vercel.app/"},
+{url:img2,title:"TV-shows", gitHub:"https://github.com/avishayavr/TV-Shows", liveDemo:"https://github.com/avishayavr/TV-Shows"}]
 
 
 const Projects = () => {
